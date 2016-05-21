21d0aefd377871b5dfd6350a76289f103b39738c
srinivasaraokandregula <seenukandregula@gmail.com>
2016-05-21 17:14:56 +0000

    node js assessment commiting

---
+++ js/gdp.js
@@ -0,0 +1,38 @@
+var fs = require("fs");
+var csvData = fs.readFileSync("csv/data.csv");
+var stringData=csvData.toString();
+var dataArray= stringData.split('\n');
+var header=dataArray[0].split(',');
+var rowsLength=dataArray.length;
+var colsLength=header.length;
+var jsonArray=new Array();
+var i=0,j=0;
+for (i = 1; i < rowsLength-1; i++){
+var obj = {};
+var temp,columnIndex;
+   var newLine=dataArray[i].split(',');
+  var nextLine=dataArray[i+1].split(',');
+   var newLine=dataArray[i].split(',');
+    if(newLine[0] !="European Union" && newLine[0] !="World"){
+      for (j = 0; j< colsLength; j++)
+      {
+        if(header[j]== "PP2013" || header[j]=="CountryName"){
+          var headerText = header[j];
+          var valueText = newLine[j];
+          if(valueText !=null && valueText !=""){
+              obj[headerText] = valueText;
+          }
+
+        }
+
+      };
+    }
+    console.log(obj);
+    jsonArray.push(obj);
+
+  //console.log(jsonArray);
+};
+fs.writeFile(process.cwd()+"/json/pp2013.json", JSON.stringify(jsonArray,undefined, 1), function (err) {
+if (err) throw err;
+console.log('Done');
+});
---
+++ js/gdpGraph.js
@@ -0,0 +1,114 @@
+/******************************************************************************
+Author : Srinivasarao Kandregula
+
+*******************************************************************************/
+//see D3 Margin convention : http://bl.ocks.org/mbostock/3019563o
+var margin={ top:20, right:10, bottom: 100, left:40},
+    width=700 - margin.right-margin.left,
+    height=500- margin.top-margin.bottom;
+
+/******************************************************************************
+Define SVG
+Still confused  about SVG? see the chapter -3
+The 'g' element used as a container for grouping objects,
+The SVG will be in "lightgrey" background to help to visualize it.
+see
+*****************************************************************************/
+var svg_gdp=d3.select('#gdp')
+        .append('svg')
+        .attr({
+        "width": width + margin.right + margin.left,
+        "height": height + margin.top + margin.bottom
+        })
+        .append('g')
+            .attr("transform", "translate(" + margin.left + ',' + margin.right + ')');
+
+/*****************************************************************************
+SCALE and AXIS are two different methods of d3.
+See D3API referance for info on AXIS and SCALE.
+See d3 api referance to understand different between Oridnal vs linear SCALE
+******************************************************************************/
+var xScale=d3.scale.ordinal()
+.rangeRoundBands([0,width], 0.1);
+var yScale=d3.scale.linear()
+.range([height, 0]);
+//define axis
+var xAxis=d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom");
+var yAxis=d3.svg.axis()
+    .scale(yScale)
+    .orient("left");
+
+
+/******************************************************************************
+TO understand how toimport data.See d3 api referance on json file.
+Understand the different between .tsv ,csv and json files,
+To import .tsv and csv file use d3.tsv or d3.csv(), respectively
+*****************************************************************************/
+d3.json("json/gdp.json", function(error, data){
+    if(error){
+    console.log("Error");
+    }
+
+  /***************************************************************************
+  Convert data with necessary . we want to make our graph value are represented
+  as integer ratherthan the string. Use "+" before the variable to convert a string
+  representing of number to an actual number. Sometimes time the data will be number
+  format, but when in doubt use "+" to avoid issues
+
+  ****************************************************************************/
+    data.forEach(function(d){
+        d.GDP2013= +d.GDP2013;
+        d.CountryName=d.CountryName;
+        console.log(d.GDP2013);
+    });
+    data.sort(function(a,b){
+    return b.GDP2013- a.GDP2013;
+    });
+// specify the domains of x and y scales
+xScale.domain(data.map(function(d){
+    return d.CountryName;
+}));
+yScale.domain([0, d3.max(data, function(d){
+    return d.GDP2013;
+})]);
+//draw the bars
+svg_gdp.selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr("height", 0)
+    .attr("y", height)
+    .transition().duration(3000)
+    .delay(function(d,i){
+        return i*200;
+    })
+    .attr({
+        'x': function(d){
+        return xScale(d.CountryName);
+        },
+        'y': function(d){
+        return yScale(d.GDP2013);
+        },
+        "width": xScale.rangeBand(),
+        "height": function(d){
+        return height-yScale(d.GDP2013);
+        }
+    });
+// append x axis and y axis
+svg_gdp.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0, " + height + ")")
+    .call(xAxis)
+    .selectAll('text')
+    .attr("transform", "rotate(-60)")
+    .attr("dx","-0.8em")
+    .attr("dy", "-0;.25em")
+    .style("text-anchor","end")
+    .style("font-size", "12px");
+svg_gdp.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .style("font-size", "12px");
+});
